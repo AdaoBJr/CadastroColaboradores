82e4ab8b76c7d685c041ee0a02f86959bb1426ae
Junior030 <juniorhenrique030@gmail.com>
2021-06-01 18:40:33 -0300

    Implementando rotas das pages

--- cadastro-colaboradores/src/App.js
+++ cadastro-colaboradores/src/App.js
@@ -1,9 +1,18 @@
 import Register from "./Pages/Register";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Register />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/employes" component={Employes} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </div>
   );
 }
